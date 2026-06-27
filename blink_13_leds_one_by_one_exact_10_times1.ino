2c045c06d9ac8eeb45d36a1ed1b3fdd971f528e2
mukulbindal30 <mukulbindal30@gmail.com>
2026-06-27 14:20:05 +0530

    Add files via upload

---
+++ blink_13_leds_one_by_one_exact_10_times1.ino
@@ -0,0 +1,26 @@
+const int n = 14;
+int pins[n] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13} ;
+void setup()
+{
+  for(int i = 0 ; i <= n-1 ; i++){
+    pinMode(pins[i] , OUTPUT);
+  }
+}
+
+void loop()
+{
+  int k = 1 ;
+  while (k <= 10){
+ 
+      for(int j = 0 ; j<= n-1 ; j++){
+      digitalWrite(pins[j] , HIGH);
+      delay(1000) ;
+      digitalWrite(pins[j] , LOW);
+    }
+    k++ ;
+     
+  }
+  while(true){
+  
+  }
+}
